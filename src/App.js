8b30f66eea771bec711254e9e469b0ab5c96c61b
Dmitry <dmitry.pleshkanovskiy@gmail.com>
2022-04-12 23:11:52 +0200

    Initial react project setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App-logo">
+          <i className="wi wi-day-sunny"></i>
+        </div>
+        <p>Yet another weather app</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
