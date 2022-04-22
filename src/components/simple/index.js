e195dad9a902e128eb3b7d23b6b9215088d18f5f
Dmitry <dmitry.pleshkanovskiy@gmail.com>
2022-04-22 18:40:07 +0200

    Basic components - index file added

---
+++ src/components/simple/index.js
@@ -0,0 +1,5 @@
+export { default as Input } from "./Input";
+export { default as Button } from "./Button";
+export { default as Loader } from "./Loader";
+export { default as Panel } from "./Panel";
+export { default as Container } from "./Container";
