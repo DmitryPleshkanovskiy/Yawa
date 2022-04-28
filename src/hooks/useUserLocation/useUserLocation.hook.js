67d2e6388e90823119d287395fd6cee99ea41992
Dmitry <dmitry.pleshkanovskiy@gmail.com>
2022-04-28 08:16:07 +0200

    Settings - location hook implemented

---
+++ src/hooks/useUserLocation/useUserLocation.hook.js
@@ -0,0 +1,110 @@
+import { useEffect, useState } from "react";
+
+import { defaultUserLocation } from "screens/Forecast/Forecast";
+
+export const useUserLocation = () => {
+  const [state, setState] = useState({
+    userLocation: null,
+    isUserLocationLoading: false,
+  });
+
+  const { userLocation, isUserLocationLoading } = state;
+
+  const setUserLocation = ({ lat, lon }) => {
+    setState((prevState) => ({ ...prevState, userLocation: { lat, lon } }));
+
+    localStorage.setItem(
+      "userLocation",
+      JSON.stringify({
+        lat,
+        lon,
+      })
+    );
+  };
+
+  const getLocationFromNavigator = () =>
+    new Promise((resolve, reject) => {
+      window?.navigator?.geolocation?.getCurrentPosition(
+        (data) => {
+          const { latitude, longitude } = data?.coords || {};
+          resolve({ lat: latitude, lon: longitude });
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
+
+  const setLocationFromNavigator = () => {
+    setState((prevState) => ({
+      ...prevState,
+      isUserLocationLoading: true,
+    }));
+
+    getLocationFromNavigator()
+      .then(({ lat, lon }) => {
+        localStorage.setItem(
+          "userLocation",
+          JSON.stringify({
+            lat,
+            lon,
+          })
+        );
+
+        setState((prevState) => ({
+          ...prevState,
+          isUserLocationLoading: false,
+          userLocation: { lat, lon },
+        }));
+      })
+      .catch((error) => {
+        localStorage.setItem(
+          "userLocation",
+          JSON.stringify({ ...defaultUserLocation })
+        );
+
+        setState((prevState) => ({
+          ...prevState,
+          userLocation: {
+            lat: defaultUserLocation.lat,
+            lon: defaultUserLocation.longitude,
+          },
+          isUserLocationLoading: false,
+          userLocationError: error,
+        }));
+
+        // TODO: show message/notification to enable geolocation
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    if (!userLocation) {
+      let parsedUserLocation;
+
+      const storedUserLocation = localStorage.getItem("userLocation");
+
+      if (storedUserLocation) {
+        try {
+          parsedUserLocation = JSON.parse(storedUserLocation);
+        } catch (error) {
+          setLocationFromNavigator();
+        }
+
+        setState((prevState) => ({
+          ...prevState,
+          userLocation: parsedUserLocation,
+        }));
+      } else {
+        setLocationFromNavigator();
+      }
+    }
+  }, []);
+
+  return {
+    userLocation,
+    setUserLocation,
+    isUserLocationLoading,
+    getLocationFromNavigator,
+  };
+};
